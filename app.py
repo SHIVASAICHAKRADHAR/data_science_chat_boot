1858fdbfe3c6b8b974d075faca0417a267800820
Shiva Sai Chakradhar <152002187+SHIVASAICHAKRADHAR@users.noreply.github.com>
2025-03-11 09:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -78,9 +78,11 @@ if st.button("Send ✉️") and user_input:
 
     # Display AI response
     st.session_state["messages"].append(("ai", response))
-    st.markdown(f"🤖 **AI:** {response}")
+
+st.markdown(f"🤖 **AI:** {response}")
+
+st.subheader("This app is made to help the students and professionals to learn about data science Made by Suman")
 
     # Save to memory
     memory.chat_memory.add_user_message(user_input)
     memory.chat_memory.add_ai_message(response)
-st.subheader("This app is made to help the students and professionals to learn about data science Made by Suman")
